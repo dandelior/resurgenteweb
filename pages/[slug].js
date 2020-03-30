9028116101f86c6d7ca998f44d5f0e617386b410
dandelior <jberrioscifuentes@gmail.com>
2020-03-30 01:25:54 -0300

    render content 1

--- pages/[slug].js
+++ pages/[slug].js
@@ -2,16 +2,35 @@ import Head from 'next/head'
 import Header from '../parts/header'
 import HeadMeta from '../parts/head-meta'
 import Footer from '../parts/footer';
+import client from '../client'
+import groq from 'groq'
+import BlockContent from '@sanity/block-content-to-react'
+import imageUrlBuilder from '@sanity/image-url'
 
-class Index extends React.Component {
-    // constructor(props) {
-    // }
+function urlFor (source) {
+    return imageUrlBuilder(client).image(source)
+}
+
+class Article extends React.Component {
+    constructor(props) {
+        super(props)
+    }
 
     render() {
+        const {
+            slug,
+            titulo, 
+            fecha, 
+            contenido, 
+            imagenDestacada, 
+            nombreTag, 
+            nombreAutor
+        } = this.props
+
         return (
             <>
                 <Head>
-                    <title>Post — Fe y Cultura</title>
+                    <title>{titulo} — Fe y Cultura</title>
                     <HeadMeta/>
                     <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700|Merriweather:300,300i,700,700i&display=swap" rel="stylesheet"></link>
                     <link href="/css/hamburger.min.css" rel="stylesheet"></link>
@@ -25,25 +44,25 @@ class Index extends React.Component {
 
                     <header className="article-header">
                         <h6 className="hashtags">
-                            <a href="#" title="nombre" className="tag tag-id slug">#tag</a>
+                            <a href="#" title="nombre" className="tag tag-id slug">#{nombreTag}</a>
                         </h6>
                         <h1 className="article-title">
-                            Título del Post
+                            {titulo}
                         </h1>
                         <h6 className="date-author">
-                            <span>por 
-                                <a href="#">nombre del autor</a>
+                            <span>por <a href="#">{nombreAutor}</a>
                             </span>
                         </h6>
                     </header>
                     
                     <div className="img pd-lr">
-                        <div className="img-inner" style={{ backgroundImage: 'url()' }}></div>
+                        <div className="img-inner" style={{ backgroundImage: 'url(' + urlFor(imagenDestacada).url() + ')' }}></div>
                     </div>
 
                     <div className="article-body pd-lr">
 
-                        Contenido
+                        {/* Contenido */}
+                        <BlockContent blocks={contenido} {...client.config()} />
 
                     </div>
 
@@ -57,4 +76,22 @@ class Index extends React.Component {
     }
 }
 
-export default Index;
+const query = groq`*[_type == "articulo" && slug.current == $slug][0]{
+    titulo, 
+    fecha, 
+    contenido, 
+    imagenDestacada, 
+    "nombreTag": tag->nombre, 
+    "nombreAutor": autor->nombre, 
+    slug
+  }`
+
+Article.getInitialProps = async function (context) {
+    // It's important to default the slug so that it doesn't return "undefined"
+    const { slug = "" } = context.query
+    return await client.fetch(query, {
+        slug
+    })
+}
+
+export default Article;
