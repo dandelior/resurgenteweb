6f01564d3694e75d2c07ec0e1e2269d3fd06d370
dandelior <jberrioscifuentes@gmail.com>
2020-11-19 17:58:46 -0300

    beta branch

--- pages/tag/[slug].js
+++ pages/tag/[slug].js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import Head from 'next/head'
 import Header from '../../parts/header'
 import HeadMeta from '../../parts/head-meta'
@@ -12,71 +13,91 @@ function urlFor (source) {
     return imageUrlBuilder(client).image(source)
 }
 
-function Tag(props) {
+class Tag extends Component {
 
-    const { 
-        articulos  = [] 
-    } = props
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        const { articulos  = [] } = this.props;
 
-    const nombreTag = articulos[0].nombre
-    const articulosTag = articulos[0].articulo
-
-    return (
-        <>
-            <Head>
-                <title>#{nombreTag} — Resurgente</title>
-                <HeadMeta/>
-                <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700|Merriweather:300,300i,700,700i&display=swap" rel="stylesheet"></link>
-                <link href="/css/hamburger.min.css" rel="stylesheet"></link>
-                <link href="/css/app.css" rel="stylesheet"></link>
-            </Head>
-
-            <Header />
-
-            <section className="grid-articles grid-on-category">
-                <div className="grid-articles-wrapper pd-lr">
-                    <header className="section-header">
-                        <h6 className="section-title">#{nombreTag}</h6>
-                    </header>
-                    <div className="articles-grid">
-
-                        {articulosTag.map(
-                            ({ _id, titulo, fecha, imagenDestacada, nombreAutor, slugAutor, slug }) =>
-                                slug && (
-                                    <div className="item" key={slug.current}>
-                                        <Link href="/[slug]" as={`/${slug.current}`}>
-                                            <a>
-                                                <div className="img" style={{ backgroundImage: 'url('+urlFor(imagenDestacada).url()+')' }}></div>
-                                            </a>
-                                        </Link>
-                                        <div className="data">
-                                            <Link href="/[slug]" as={`/${slug.current}`} key={slug.current}>
+        const nombreTag = articulos[0].nombre
+        const articulosTag = articulos[0].articulo
+    
+        return (
+            <>
+                <Head>
+                    <title>#{nombreTag} — Resurgente</title>
+                    <HeadMeta/>
+                    <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700|Merriweather:300,300i,700,700i&display=swap" rel="stylesheet"></link>
+                    <link href="/css/hamburger.min.css" rel="stylesheet"></link>
+                    <link href="/css/app.css" rel="stylesheet"></link>
+                </Head>
+    
+                <Header />
+    
+                <section className="grid-articles grid-on-category">
+                    <div className="grid-articles-wrapper pd-lr">
+                        <header className="section-header">
+                            <h6 className="section-title">#{nombreTag}</h6>
+                        </header>
+                        <div className="articles-grid">
+    
+                            {articulosTag.map(
+                                ({ _id, titulo, fecha, imagenDestacada, nombreAutor, slugAutor, slug }) =>
+                                    slug && (
+                                        <div className="item" key={slug.current}>
+                                            <Link href="/[slug]" as={`/${slug.current}`}>
                                                 <a>
-                                                    <h3 className="article-title">
-                                                        {titulo}
-                                                    </h3>
+                                                    <div className="img" style={{ backgroundImage: 'url('+urlFor(imagenDestacada).url()+')' }}></div>
                                                 </a>
                                             </Link>
-                                            <h6 className="author">
-                                                {moment(fecha).locale('es').format('LL')} — por <Link href="/autor/[slugAutor]" as={`/autor/${slugAutor.current}`}>
-                                                    <a>{nombreAutor}</a>
+                                            <div className="data">
+                                                <Link href="/[slug]" as={`/${slug.current}`} key={slug.current}>
+                                                    <a>
+                                                        <h3 className="article-title">
+                                                            {titulo}
+                                                        </h3>
+                                                    </a>
                                                 </Link>
-                                            </h6>
+                                                <h6 className="author">
+                                                    {moment(fecha).locale('es').format('LL')} — por <Link href="/autor/[slugAutor]" as={`/autor/${slugAutor.current}`}>
+                                                        <a>{nombreAutor}</a>
+                                                    </Link>
+                                                </h6>
+                                            </div>
                                         </div>
-                                    </div>
-                                )
-                        )}
-
+                                    )
+                            )}
+    
+                        </div>
+    
                     </div>
-
-                </div>
-                {/* pagination */}
-            </section>
-
-            <Footer />
-        </>
-    )
+                    {/* pagination */}
+                    {/* <button onClick={this.loadMore}>Cargar mas...</button>   */}
+                    <button>Cargar mas...</button>  
+                </section>
+    
+                <Footer />
+            </>
+        )
+    }
 }
+
+const loadMoreQuery = `
+    *[_type == "tag" && slug.current == "fe"] {
+        nombre,
+        "articulo": *[_type == "articulo" && references(^._id)] | order(_updatedAt desc){
+            titulo,
+            fecha,
+            imagenDestacada,
+            "nombreAutor": autor->nombre, 
+            "slugAutor": autor->slug, 
+            slug
+        }[10...18]
+    }
+`
 
 const theQuery = `
     *[_type == "tag" && slug.current == $slug] {
